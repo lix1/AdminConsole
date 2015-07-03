69b1eabfd31bc187d23db4c0f37ab0938b239dd5
lix1@umbc.edu <lix1@umbc.edu>
2015-07-03 15:34:12 -0400

    init mongo

---
+++ app/models/intro.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  path = require('path');
+
+var IntroSchema = new Schema({
+  title:          { type: String },
+  description:    { type: String },
+  index:          { type: Number, 'default': 0 }
+});
+
+module.exports = mongoose.model('Image', ImageSchema);
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express'),
   config = require('./server/configure'),
-  app = express();
+  app = express(),
+  mongoose = require('mongoose');
 
 app.set('appName', 'RCI');
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
+
+mongoose.connect('mongodb://localhost/RCI');
+mongoose.connection.on('open', function() {
+  console.log('Mongoose connected.');
+});
 
 var server = app.listen(app.get('port'), function() {
   console.log('Server up: http://localhost:' + app.get('port'));
